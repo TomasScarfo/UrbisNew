7b3c289278f7586585690c3fa3c4d3156d084e7a
tomas scarfo <tomas.scarfo@gmail.com>
2022-11-04 16:59:18 -0300

    SDsd

---
+++ trabajo/src/assets/js/Opciones.js
@@ -0,0 +1,75 @@
+const entradas = [
+    {
+        producto: 'Tortilla',
+        descripcion: 'Tortilla de papa, verdura o acelga',
+        img: '',
+        precio: 700
+    },
+    {
+        producto: 'Buñuelos',
+        descripcion: '8 buñuelos caseros al estilo Urbis',
+        img: '',
+        precio: 700
+    },{
+        producto: 'Gambas',
+        descripcion: 'Al ajillo',
+        img: '',
+        precio: 1000
+    },{
+        producto: 'Lengua',
+        descripcion: 'Al escabeche',
+        img: '',
+        precio: 1000
+    },
+]
+
+const principal = [
+    {
+        producto: 'Milanesa',
+        descripcion: 'De pollo o ternera con guarnición',
+        img: '',
+        precio: 1500
+    },
+    {
+        producto: 'Ñoquis',
+        descripcion: 'Servidos con salsa a elección',
+        img: '',
+        precio: 1300
+    },{
+        producto: 'Pollo',
+        descripcion: 'Pollo al horno con guarnición a elección',
+        img: '',
+        precio: 1300
+    },{
+        producto: 'Carne',
+        descripcion: 'Carne al horno con guarnición a elección',
+        img: '',
+        precio: 1800
+    },
+]
+
+const postre = [
+    {
+        producto: 'Flan',
+        descripcion: 'Flan casero simple o mixto',
+        img: '',
+        precio: 500
+    },
+    {
+        producto: 'Vigilante',
+        descripcion: 'Membrillo o batata con queso',
+        img: '',
+        precio: 500
+    },{
+        producto: 'Pollo',
+        descripcion: 'Pollo al horno con guarnición a elección',
+        img: '',
+        precio: 1300
+    },{
+        producto: 'Carne',
+        descripcion: 'Carne al horno con guarnición a elección',
+        img: '',
+        precio: 1800
+    },
+]
+
